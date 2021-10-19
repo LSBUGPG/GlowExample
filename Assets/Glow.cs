7270aa37264e21ff380c9eeff780bb7fa9c4e92b
Paul Sinnett <paul.sinnett@gmail.com>
2021-10-19 01:04:08 +0100

    pulse material

--- Assets/Glow.cs
+++ Assets/Glow.cs
@@ -5,17 +5,24 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class Glow : MonoBehaviour
 {
-    public PostProcessVolume volume;
+    new public Renderer renderer;
+    public Color colour;
     public float bloom;
     public float speed = 1.0f;
     float time = 0.0f;
+    Material material;
+
+    void Start()
+    {
+        material = renderer.material;
+        renderer.material = material;
+    }
 
     void Update()
     {
         time += Time.deltaTime * speed;
-        PostProcessProfile profile = volume.profile;
-        Bloom bloomEffect = profile.GetSetting<Bloom>();
         float pulse = Mathf.Sin(time) * 0.5f + 0.5f;
-        bloomEffect.intensity.Override(pulse * bloom);
+        colour.r = pulse * bloom;
+        material.SetColor("_EmissionColor", colour);
     }
 }
